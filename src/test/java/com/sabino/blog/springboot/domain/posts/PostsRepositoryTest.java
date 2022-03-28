3cb234ed97390c3679f9c964c29ff80397688a58
MrKeeplearning <jaeseok1115@gmail.com>
2022-03-29 00:00:22 +0900

    Feat: Spring Data Jpa 적용 및 테스트코드 작성, 등록/수정/조회 API 추가

---
+++ src/test/java/com/sabino/blog/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.sabino.blog.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void loadSavedPosts() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("sabino@gmail.com")
+                        .build());
+
+        //when
+        List<Posts> postsLists = postsRepository.findAll();
+
+        //then
+        Posts posts = postsLists.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+}
